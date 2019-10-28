6126bf3d23d6c629d32a20bc263660d7992b59fc
Rlucer <lucero.ricardo@gmail.com>
2019-10-28 20:14:06 -0300

    subiendo

---
+++ mysql/src/gestionBD/GestionBD.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Girona 03
+ */
+public class GestionBD {
+
+    String user = "rlucero";
+    String pass = "12345678";
+    String db_name = "paja";
+
+    private static Connection Conexion;
+
+    
+    
+    
+    public void conecta() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion = DriverManager.getConnection("jdbc:mysql://201.238.207.58:3306/" + db_name, user, pass);
+            System.out.println("Se ha iniciado la conexión con el servidor de forma exitosa");
+
+           
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, ex + "error1");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex + "error2");
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    public void closeConnection() {
+        try {
+            Conexion.close();
+            System.out.println("Se ha finalizado la conexión con el servidor");
+        } catch (SQLException ex) {
+           Logger.getLogger(GestionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void createTable(String name) throws ClassNotFoundException {
+        try {
+
+            String Query = "CREATE TABLE " + name + ""
+                    + "(ID VARCHAR(25),Nombre VARCHAR(50), Apellido VARCHAR(50),"
+                    + " Edad VARCHAR(3), Sexo VARCHAR(1))";
+            JOptionPane.showMessageDialog(null, "Se ha creado la base de tabla " + name + " de forma exitosa");
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(GestionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void insertData(String table_name, String ID, String name, String lastname, String age, String gender) {
+        try {
+            String Query = "INSERT INTO " + table_name + " VALUES("
+                    + "\"" + ID + "\", "
+                    + "\"" + name + "\", "
+                    + "\"" + lastname + "\", "
+                    + "\"" + age + "\", "
+                    + "\"" + gender + "\")";
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            JOptionPane.showMessageDialog(null, "Datos almacenados de forma exitosa");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en el almacenamiento de datos");
+        }
+    }
+
+    public void getValues(String table_name) {
+        try {
+            String Query = "SELECT * FROM " + table_name;
+            Statement st = Conexion.createStatement();
+            java.sql.ResultSet resultSet;
+            resultSet = st.executeQuery(Query);
+
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getString("ID") + " "
+                        + "Nombre: " + resultSet.getString("Nombre") + " " + resultSet.getString("Apellido") + " "
+                        + "Edad: " + resultSet.getString("Edad") + " "
+                        + "Sexo: " + resultSet.getString("Sexo"));
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en la adquisición de datos");
+        }
+    }
+
+    public void deleteRecord(String table_name, String ID) {
+        try {
+            String Query = "DELETE FROM " + table_name + " WHERE ID = \"" + ID + "\"";
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Error borrando el registro especificado");
+        }
+    }
+
+}
