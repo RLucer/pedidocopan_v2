35596d9c2a330611a7443fde7a037f07dafa38c5
“Rlucer“ <lucero.ricardo@gmail.com>
2019-12-11 09:26:10 -0300

    desde macbook modif categoria

--- AppPedidoCopan/src/principal/categoria.java
+++ AppPedidoCopan/src/principal/categoria.java
@@ -40,7 +40,7 @@ public class categoria extends javax.swing.JFrame {
         txtcat = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Categorias");
 
